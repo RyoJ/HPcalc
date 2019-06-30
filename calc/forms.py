224c206368edde36a4e7ddc83b1cdf850f183191
EC2 Default User <ec2-user@ip-172-31-40-126.us-east-2.compute.internal>
2019-06-30 23:19:57 +0000

    forms_imp_by_as_p

--- calc/forms.py
+++ calc/forms.py
@@ -1,22 +1,9 @@
 from django import forms
+from .models import Status
 
 
-class StatusForm(forms.Form):
-    hp = forms.IntegerField(
-        label='hp',
-        required=True,
-        #widget=forms.TextInput() #数値の場合は必要ない？
-    )
+class StatusForm(forms.ModelForm):
 
-    mp = forms.IntegerField(
-        label='mp',
-        required=True,
-        #widget=forms.TextInput() #数値の場合は必要ない？
-    )
-
-    event = forms.CharField(
-        label='event',
-        max_length=100,
-        required=True,
-        widget=forms.TextInput()
-    )
+    class Meta:
+        model = Status
+        fields = ('hp', 'mp', 'event')
